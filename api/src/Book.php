96491e21b292f258411169019e43c6508c6b5014
Michal Kwiatkowski <kfiatos@gmail.com>
2015-07-24 09:22:16 +0200

    Initial commit

---
+++ api/src/Book.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: michal
+ * Date: 24.07.15
+ * Time: 09:18
+   CREATE TABLE Book (id int NOT NULL AUTO_INCREMENT,
+   name VARCHAR(255), author VARCHAR(255),
+   opis VARCHAR(255),
+   PRIMARY KEY (id));
+  */
+
+class Book
+{
+
+}
+
+?>
